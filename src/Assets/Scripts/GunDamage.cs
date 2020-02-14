1ca4c72be2947af6badfc8691c83d4a86ecf8991
Bryce Winnecke <bwwinne@gmail.com>
2020-02-14 18:47:25 -0500

    added crosshair and raycast shoot to FPS debug gun to mimick longbow shot for testing

--- src/Assets/Scripts/GunDamage.cs
+++ src/Assets/Scripts/GunDamage.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class GunDamage : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float damage = 1f;
+    public float range = 100f;
+    public float impactForce = 30f;
+
+    public Camera FPSCam;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        // shoot a ray from the camera straigth forward and see if it hits any objects with colliders
+        if (Physics.Raycast(FPSCam.transform.position, FPSCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            BlobEnemy enemy = hit.transform.GetComponent<BlobEnemy>();
+            if (enemy != null)
+            {
+                enemy.TakeDamageFromEvent();
+            }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+        }
     }
 }

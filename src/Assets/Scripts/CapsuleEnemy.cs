5e9019ced14ed75f66dddcd8cc5a5ebbeb9ab3f1
Bryce Winnecke <bwwinne@gmail.com>
2020-02-29 06:18:58 -0500

    imported some assets, made capsule enemies play explosion animation when they die, added sound effect for explosion

--- src/Assets/Scripts/CapsuleEnemy.cs
+++ src/Assets/Scripts/CapsuleEnemy.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR.InteractionSystem;
 
 public class CapsuleEnemy : Enemy
 {
-    public bool HideOnHit = true;
-    public bool PhysicsOnHit = false;
+    public GameObject explosionPrefab;
+
     public float respawnTime = 3f;
+
+    private AudioSource audio;
+
+    private void Awake()
+    {
+        audio = this.gameObject.GetComponent<AudioSource>();
+    }
 
     public void TakeDamageFromEvent()
     {
@@ -16,9 +24,33 @@ public class CapsuleEnemy : Enemy
 
     private void KillEnemy()
     {
-        // don't destroy these from the scene, we may as well just turn them back on later
-        this.gameObject.SetActive(false);
-
         RotatingEnemyController.Instance?.OnEnemyDeath();
+
+        StartCoroutine(Explode());
+
+        // don't destroy these from the scene, we may as well just turn them back on later
+        this.gameObject.GetComponent<MeshRenderer>().enabled = false;
+        this.gameObject.GetComponent<CapsuleCollider>().enabled = false;
+
+        GameObject arrow = gameObject.GetComponentInChildren<Arrow>().gameObject;
+        Destroy(arrow);
+
+        
+    }
+
+    private IEnumerator Explode()
+    {
+        audio.Play(0);
+
+        GameObject explosion = Instantiate(explosionPrefab);
+        explosion.transform.position = this.gameObject.transform.position;
+
+        yield return new WaitForSeconds(2f);
+
+        Destroy(explosion);
+
+        this.gameObject.GetComponent<MeshRenderer>().enabled = true;
+        this.gameObject.GetComponent<CapsuleCollider>().enabled = true;
+        this.gameObject.SetActive(false);
     }
 }
